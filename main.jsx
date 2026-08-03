aaabecc9e528bf1225e54f78d4ae3afcc173d3d8
YHSONG1109 <ulove1109@gmail.com>
2026-08-03 18:26:04 +0900

    Delete main.jsx

--- main.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import "./storage.js";          // window.storage 를 먼저 준비합니다
-import App from "./App.jsx";
-
-createRoot(document.getElementById("root")).render(<App />);
